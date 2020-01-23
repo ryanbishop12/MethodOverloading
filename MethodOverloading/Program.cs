3484fadd27bc94faea22cfbb4f7e9d3241da09c0
thatpersoninthemirror <ryan.bishop258@gmail.com>
2020-01-23 09:04:26 -0600

    Add project files.

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(25, 46, true));
+        }
+        
+        private static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        private static string Add(int a, int b, bool c)
+        {
+            int result = a + b;
+            string bills = " dollar";
+            if (c)
+            {
+                if (result > 1)
+                    bills += "s";
+                return result + bills;
+            }
+            return result.ToString();
+        }
+    }
+}
